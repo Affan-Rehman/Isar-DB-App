bebd7624293343b18ad098fe1a71f8dab1b8cd1b
Affan Rehman <112381046+Affan-Rehman@users.noreply.github.com>
2024-01-18 16:02:00 +0500

    create routine consistency handled by self

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,136 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 import 'package:isar_db_app/collections/category.dart';
 import 'package:isar_db_app/collections/routine.dart';
-import 'package:isar_db_app/screens/create_routine.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:isar_db_app/screens/create_routine.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final dir = await getApplicationSupportDirectory();
   final isar =
       await Isar.open([RoutineSchema, CategorySchema], directory: dir.path);
-  runApp(CreateRoutine(
-    isar: isar,
-  ));
+  runApp(MyApp(isar: isar));
+}
+
+class MyApp extends StatelessWidget {
+  final Isar isar;
+  const MyApp({Key? key, required this.isar}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Routine app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainPage(isar: isar),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  final Isar isar;
+  const MainPage({Key? key, required this.isar}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List<Routine>? routines;
+  @override
+  void initState() {
+    super.initState();
+    _readRoutines();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Routine"),
+        actions: [
+          IconButton(
+              onPressed: () async {
+                var result = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            CreateRoutine(isar: widget.isar)));
+                log(result.toString());
+                if (result == "ok") {
+                  setState(() {
+                    _readRoutines();
+                  });
+                }
+              },
+              icon: const Icon(Icons.add))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: _buildWidgets()),
+      ),
+    );
+  }
+
+  List<Widget> _buildWidgets() {
+    List<Widget> x = [];
+
+    for (int i = 0; i < routines!.length; i++) {
+      x.add(Card(
+        elevation: 4.0,
+        child: ListTile(
+            title:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0, bottom: 2.0),
+                child: Text(
+                  routines![i].title,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 5.0),
+                child: RichText(
+                    text: TextSpan(
+                        style: const TextStyle(color: Colors.black),
+                        children: [
+                      const WidgetSpan(child: Icon(Icons.schedule, size: 16)),
+                      TextSpan(text: routines![i].startTime)
+                    ])),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 5.0),
+                child: RichText(
+                    text: TextSpan(
+                        style:
+                            const TextStyle(color: Colors.black, fontSize: 12),
+                        children: [
+                      const WidgetSpan(
+                          child: Icon(
+                        Icons.calendar_month,
+                        size: 16,
+                      )),
+                      TextSpan(text: routines![i].day)
+                    ])),
+              )
+            ]),
+            trailing: const Icon(Icons.keyboard_arrow_right)),
+      ));
+    }
+    return x;
+  }
+
+  _readRoutines() async {
+    final routineCollection = widget.isar.routines;
+    final getRoutines = await routineCollection.where().findAll();
+    setState(() {
+      routines = getRoutines;
+    });
+  }
 }
--- lib/screens/create_routine.dart
+++ lib/screens/create_routine.dart
@@ -37,121 +37,138 @@ class _CreateRoutineState extends State<CreateRoutine> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xffffffff),
-      appBar: AppBar(title: const Text("Create routine")),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            const Text(
-              "Category",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.7,
-                  child: DropdownButton(
-                    focusColor: const Color(0xffffffff),
-                    dropdownColor: const Color(0xffffffff),
-                    isExpanded: true,
-                    value: dropdownValue,
-                    icon: const Icon(Icons.keyboard_arrow_down),
-                    items: categories
-                        ?.map<DropdownMenuItem<Category>>((Category nvalue) {
-                      return DropdownMenuItem<Category>(
-                          value: nvalue, child: Text(nvalue.category));
-                    }).toList(),
-                    onChanged: (Category? newValue) {
-                      setState(() {
-                        dropdownValue = newValue!;
-                      });
-                    },
-                  ),
-                ),
-                IconButton(
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) => AlertDialog(
-                                title: const Text("New Category"),
-                                content: TextFormField(
-                                    controller: _newCatController),
-                                actions: [
-                                  ElevatedButton(
-                                      onPressed: () {
-                                        if (_newCatController.text.isNotEmpty) {
-                                          _addCategory(widget.isar);
-                                        }
-                                      },
-                                      child: const Text("Add"))
-                                ],
-                              ));
-                    },
-                    icon: const Icon(Icons.add))
-              ],
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 10.0),
-              child:
-                  Text("Title", style: TextStyle(fontWeight: FontWeight.bold)),
-            ),
-            TextFormField(
-              controller: _titleController,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 10.0),
-              child: Text("Start Time",
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.7,
-                  child: TextFormField(
-                    controller: _timeController,
-                    enabled: false,
-                  ),
-                ),
-                IconButton(
-                    onPressed: () {
-                      _selectedTime(context);
-                    },
-                    icon: const Icon(Icons.calendar_month))
-              ],
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 10.0),
-              child: Text("Day", style: TextStyle(fontWeight: FontWeight.bold)),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.7,
-              child: DropdownButton(
-                isExpanded: true,
-                value: dropdownDay,
-                icon: const Icon(Icons.keyboard_arrow_down),
-                items: days.map<DropdownMenuItem<String>>((String day) {
-                  return DropdownMenuItem<String>(value: day, child: Text(day));
-                }).toList(),
-                onChanged: (String? newDay) {
-                  setState(() {
-                    dropdownDay = newDay!;
-                  });
-                },
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pop(context, "ok");
+        return true;
+      },
+      child: Scaffold(
+        backgroundColor: const Color(0xffffffff),
+        appBar: AppBar(
+          title: const Text("Create routine"),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context, "ok");
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              const Text(
+                "Category",
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
-            ),
-            Align(
-                alignment: Alignment.center,
-                child: ElevatedButton(
-                    onPressed: () {
-                      addRoutine();
-                    },
-                    child: const Text("Add")))
-          ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    child: DropdownButton(
+                      focusColor: const Color(0xffffffff),
+                      dropdownColor: const Color(0xffffffff),
+                      isExpanded: true,
+                      value: dropdownValue,
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      items: categories
+                          ?.map<DropdownMenuItem<Category>>((Category nvalue) {
+                        return DropdownMenuItem<Category>(
+                            value: nvalue, child: Text(nvalue.category));
+                      }).toList(),
+                      onChanged: (Category? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                      },
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                                  title: const Text("New Category"),
+                                  content: TextFormField(
+                                      controller: _newCatController),
+                                  actions: [
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          if (_newCatController
+                                              .text.isNotEmpty) {
+                                            _addCategory(widget.isar);
+                                          }
+                                        },
+                                        child: const Text("Add"))
+                                  ],
+                                ));
+                      },
+                      icon: const Icon(Icons.add))
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0),
+                child: Text("Title",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+              TextFormField(
+                controller: _titleController,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0),
+                child: Text("Start Time",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    child: TextFormField(
+                      controller: _timeController,
+                      enabled: false,
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        _selectedTime(context);
+                      },
+                      icon: const Icon(Icons.calendar_month))
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0),
+                child:
+                    Text("Day", style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: DropdownButton(
+                  isExpanded: true,
+                  value: dropdownDay,
+                  icon: const Icon(Icons.keyboard_arrow_down),
+                  items: days.map<DropdownMenuItem<String>>((String day) {
+                    return DropdownMenuItem<String>(
+                        value: day, child: Text(day));
+                  }).toList(),
+                  onChanged: (String? newDay) {
+                    setState(() {
+                      dropdownDay = newDay!;
+                    });
+                  },
+                ),
+              ),
+              Align(
+                  alignment: Alignment.center,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        addRoutine();
+                      },
+                      child: const Text("Add")))
+            ]),
+          ),
         ),
       ),
     );
